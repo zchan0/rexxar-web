e10186ffa6b2eedccb7f72657f9ac844f1daeecf
Cc Zheng <zchan0@outlook.com>
2019-05-29 22:48:12 +0800

    update example dist

--- example/tasks/globals/consts.js
+++ example/tasks/globals/consts.js
@@ -2,7 +2,7 @@ import argv from '../utils/argv';
 
 export const distDir = 'dist';
 
-export const remotePath = 'https://raw.githubusercontent.com/douban/rexxar-web/master/example/dist';
+export const remotePath = 'https://github.com/zchan0/rexxar-web/tree/master/example/dist';
 
 export const serveIp = argv.ip || '0.0.0.0';
 export const servePort = argv.p || '8080';
